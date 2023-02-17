b52b343f2627ce60396dc9f15665d10094053f38
ARUN BALAJI <53110175+ArunBalajiR@users.noreply.github.com>
2023-02-17 09:52:20 +0000

    finalfeb2

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from flask import Flask, request,jsonify
 import fasttext
 import re
 import joblib
-# Load trained model
-clf = joblib.load('./models/lob_predictor/lobmodel.joblib')
-tfidf = joblib.load('./models/lob_predictor/lob_tfidf_model.joblib')
-mlb = joblib.load('./models/lob_predictor/lob_mlb_model.joblib')
+
 
 
 app = Flask(__name__)
@@ -14,29 +11,32 @@ app = Flask(__name__)
 def hello():
     return "NLP DOCUMENT CLASSIFICATION API"
 
-@app.route('/gettags', methods=['GET'])
-def predictLOBsandTags():
-    try:
-        text = request.json['text']
-        preprocessed_text = preprocess(text)
-        print(preprocessed_text)
-        return "hi"
-    except Exception as e:
-        return jsonify({"error": str(e)})
 
-# Load the FastText model from a pickle file using joblib
-loaded_model = fasttext.load_model("./models/fasttext_model.bin");
+# Load trained model
+clf = joblib.load('./models/lob_predictor/lobmodel.joblib')
+tfidf = joblib.load('./models/lob_predictor/lob_tfidf_model.joblib')
+mlb = joblib.load('./models/lob_predictor/lob_mlb_model.joblib')
+
+tclf = joblib.load('./models/tags_predictor/tagsmodel.joblib')
+ttfidf = joblib.load('./models/tags_predictor/tags_tfidf_model.joblib')
+tmlb = joblib.load('./models/tags_predictor/tags_mlb_model.joblib')
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        text = request.json['text']
-        preprocessed_text = preprocess(text)
-        prediction = loaded_model.predict(preprocessed_text)
-        prediction_label, prediction_probability = prediction[0][0], prediction[1][0]
-        label = prediction_label.replace('__label__', '')
+        text = [request.json['text']]
+        # LOB PREDICTION
+        xt = tfidf.transform(text)
+        y_hat = clf.predict(xt)
+        lobtags = mlb.inverse_transform(y_hat)
+        # tags prediction
+        xxt = ttfidf.transform(text)
+        yy_hat = tclf.predict(xxt)
+        tags = tmlb.inverse_transform(yy_hat)
         return jsonify({
-            "prediction": label,
-            "probability": round(prediction_probability * 100, 2)
+            "tags": list(tags[0]),
+            "lobs": list(lobtags[0])
         })
     except Exception as e:
         return jsonify({"error": str(e)})
