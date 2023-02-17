685001a636ab9d5eb9720b92b24da66a65a40681
ARUN BALAJI <53110175+ArunBalajiR@users.noreply.github.com>
2023-02-17 08:41:55 +0000

    added

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import fasttext
 import re
 import joblib
 # Load trained model
-clf = joblib.load('./models/model.joblib')
-tfidf = joblib.load('./models/tfidf_model.joblib')
-mlb = joblib.load('./models/mlb_model.joblib')
+clf = joblib.load('./models/lob_predictor/lobmodel.joblib')
+tfidf = joblib.load('./models/lob_predictor/lob_tfidf_model.joblib')
+mlb = joblib.load('./models/lob_predictor/lob_mlb_model.joblib')
 
 
 app = Flask(__name__)
@@ -14,18 +14,13 @@ app = Flask(__name__)
 def hello():
     return "NLP DOCUMENT CLASSIFICATION API"
 
-@app.route('/gettags', methods=['POST'])
-def predictTags():
+@app.route('/gettags', methods=['GET'])
+def predictLOBsandTags():
     try:
         text = request.json['text']
         preprocessed_text = preprocess(text)
-        prediction = loaded_model.predict(preprocessed_text)
-        prediction_label, prediction_probability = prediction[0][0], prediction[1][0]
-        label = prediction_label.replace('__label__', '')
-        return jsonify({
-            "prediction": label,
-            "probability": round(prediction_probability * 100, 2)
-        })
+        print(preprocessed_text)
+        return "hi"
     except Exception as e:
         return jsonify({"error": str(e)})
 
